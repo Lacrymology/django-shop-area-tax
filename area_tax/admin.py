6658245322e2be8c1a5971881c4d89112e244260
Tomas Neme <lacrymology@gmail.com>
2012-03-15 23:44:20 -0300

    some admin details

--- area_tax/admin.py
+++ area_tax/admin.py
@@ -8,4 +8,9 @@ class AreaTaxAdmin(CountryAreaAdmin):
     Admin class for AreaTax. Constrains the data types
     """
     area_field_required = "false"
+    list_display = ("name", "country", "area", "percentage", "is_compound",
+                    "override_national")
+    list_editable = ("percentage", "is_compound", "override_national")
+    ordering = ("country", "area")
+
 admin.site.register(AreaTax, AreaTaxAdmin)
